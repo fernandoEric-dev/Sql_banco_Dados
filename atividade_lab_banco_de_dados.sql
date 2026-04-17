ca3c5f2f525aeee4dd5798bd8d846720be5356af
Fernando <fernandoeric.feds8@gmail.com>
2026-04-17 07:44:04 -0300

    Remove unnecessary SQL comments and settings

--- atividade_lab_banco_de_dados.sql
+++ atividade_lab_banco_de_dados.sql
@@ -1,31 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 17/04/2026 às 04:46
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.0.30
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Banco de dados: `atividade_lab_banco_de_dados`
---
 
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `categorias_servico`
---
+
 
 CREATE TABLE `categorias_servico` (
   `id` int(11) NOT NULL,
